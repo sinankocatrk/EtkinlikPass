beb7cb3947f3a6a012a36ac29f3e34e26312e8bf
sinankocatrk <76816180+sinankocatrk@users.noreply.github.com>
2023-10-29 22:29:48 +0300

    feat: updated register page

---
+++ core/user/forms.py
@@ -0,0 +1,19 @@
+from django import forms 
+
+from django import forms
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=50, label="Kullanıcı adı")
+    email = forms.EmailField(max_length=100, label="E-posta")
+    password = forms.CharField(max_length=20, label="Parola", widget=forms.PasswordInput)
+    confirm = forms.CharField(max_length=20, label="Parolayı Doğrula", widget=forms.PasswordInput)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm = cleaned_data.get("confirm")
+
+        if password and confirm and password != confirm:
+            raise forms.ValidationError("Parolalar eşleşmiyor")
+
+        return cleaned_data
